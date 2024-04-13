7237ea6900747f1fc1f92b1e2c2a714d47d1f8e2
Ivan Zorin <creator@localzet.com>
2024-04-13 11:12:19 +0300

    Update

--- classificator.py
+++ classificator.py
@@ -6,11 +6,21 @@ from trainer import Trainer
 
 
 class Classificator:
+    """
+    Класс Classificator для классификации названий продуктов по категориям.
+    """
+
     def __init__(self):
+        """
+        Инициализирует объект Classificator, загружая модель и векторизатор.
+        """
         self.trainer = Trainer()
         self.model, self.vectorizer = self._load_model_and_vectorizer()
 
     def _load_model_and_vectorizer(self):
+        """
+        Загружает модель и векторизатор, если они существуют, иначе начинает процесс обучения.
+        """
         if os.path.exists(self.trainer.model_path) and os.path.exists(self.trainer.vectorizer_path):
             model = joblib.load(self.trainer.model_path)
             vectorizer = joblib.load(self.trainer.vectorizer_path)
@@ -22,14 +32,20 @@ class Classificator:
             return joblib.load(self.trainer.model_path), joblib.load(self.trainer.vectorizer_path)
 
     def predict_category(self, product_name):
-        return self.predict_categories(product_name)[0]
+        """
+        Предсказывает категорию для одного названия продукта.
+        """
+        return self.predict_categories([product_name])[0]
 
-    def predict_categories(self, product_name):
+    def predict_categories(self, product_names):
+        """
+        Предсказывает категории для списка названий продуктов.
+        """
         if not self.model or not self.vectorizer:
             print('Модель или векторизатор не загружены. Пожалуйста, обучите модель.')
             return None
 
-        vectorized_name = self.vectorizer.transform([product_name]).toarray()
-        prediction = self.model.predict(vectorized_name)
+        vectorized_names = self.vectorizer.transform(product_names).toarray()
+        predictions = self.model.predict(vectorized_names)
 
-        return prediction
+        return predictions
