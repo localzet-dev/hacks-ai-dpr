396c6a62d9f669745a3aab429111197d059132a3
Ivan Zorin <creator@localzet.com>
2024-04-13 16:44:23 +0300

    Update recognizer.py

--- recognizer.py
+++ recognizer.py
@@ -7,7 +7,7 @@ from preprocessor import Preprocessor
 
 
 class Recognizer:
-    def __init__(self, model_path='bert-base-uncased'):
+    def __init__(self, model_path='bert-base-multilingual-cased'):
         self.model = BertForSequenceClassification.from_pretrained(model_path)
         self.tokenizer = BertTokenizer.from_pretrained(model_path)
         self.preprocessor = Preprocessor()
@@ -24,12 +24,11 @@ class Recognizer:
 
     def recognize(self, image_path):
         image = cv2.imread(image_path)
-
         orig_with_boxes, ROIs = self.preprocessor.preprocess(image)
         results = []
         for roi in ROIs:
-            roi_image = roi[0]
-            text, class_label = self.classify_text(roi_image)
+            text = self.image_to_string(roi[0])
+            _, class_label = self.classify_text(text)
             results.append((text, class_label))
         return orig_with_boxes, results
 
@@ -38,8 +37,8 @@ recognizer = Recognizer()
 orig_with_boxes, results = recognizer.recognize(
     'D:\\DevDrive\\localzet-dev\\hacks-ai-dpr\\assets\\subm_example\\IMG_20240329_095208778~2.jpg')
 
-for i, (_, class_label) in enumerate(results[1]):
-    print(f'ROI {i}: Class {class_label[0]} - {class_label[1]}')
+for i, (text, class_label) in enumerate(results):
+    print(f'ROI {i}: Class {class_label} - {text}')
 
 cv2.imshow('Text Detection', orig_with_boxes)
 cv2.waitKey(0)
